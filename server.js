91a77488ec90ba93087a3a16f1d48e5e38128ed8
fahimmarley5-sudo <fahimmarley5@gmail.com>
2026-09-15 04:48:07 +0400

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ ticksDataProcessor.on('newTick', (tickData) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, `0.0.0.0', () => {
+server.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
   tickDataProcessor.start();
 });
