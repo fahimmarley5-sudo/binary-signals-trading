45fb93db00a8c9743055c6564d1eead1ae02eb6a
fahimmarley5-sudo <fahimmarley5@gmail.com>
2026-09-15 06:57:39 +0400

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ tickDataProcessor.on('newTick', (tickData) => {
   });
 });
 app.get('/', (req, res) => {
-    res.send("Trading Engine Live");});
+    res.send("Trading Engine Live");};
 const PORT = process.env.PORT || 5000;
 server.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
